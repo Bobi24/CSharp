013ae6fa1a5230f1a073738869b7368d255e40c7
Borislav Motev <87946210+Bobi24@users.noreply.github.com>
2026-02-04 17:37:24 +0200

    Add files via upload

---
+++ YearCalculator.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("What is your name?");
+string Name = Console.ReadLine();
+
+Console.WriteLine("When are you born?");
+int Year = int.Parse(Console.ReadLine());
+
+Console.WriteLine(2026 - Year);
