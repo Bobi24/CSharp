f73bae1bdad11c4b3456d7bcaa03b78c65e10355
Borislav Motev <87946210+Bobi24@users.noreply.github.com>
2026-02-04 17:16:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Autobiography\r\n");
+
+string Name = "Borislav";
+Console.WriteLine(Name);
+
+string LastName = "Motev";
+Console.Write(LastName);
+
+Console.WriteLine("Personal data");
+
+double MobilePhone = 0878240880;
+Console.WriteLine(MobilePhone);
+
+string Mail = "bobimot077@mail.bg\r\n";
+Console.WriteLine(Mail);
+
+
+Console.WriteLine("Education");
+
+string Role = "Engineer in Telecommunications";
+Console.WriteLine(Role);
+
+string Degree1 = "a student in First Spanish Language High-school";
+Console.WriteLine(Degree1);
+
+string Degree2 = "a bachelor in Telecommunications in TU-Sofia";
+Console.WriteLine(Degree2);
+
+string Degree3 = "a master in International Project Management in UNWE-Sofia\r\n";
+Console.WriteLine(Degree3);
+
+Console.WriteLine("Certificates");
+
+string Cer1 = "Cambridge English Exam level A2";
+Console.WriteLine(Cer1);
+
+string Cer2 = "Cambridge English Exam level B1";
+Console.WriteLine(Cer2);
+
+string Cer3 = "C# Basics\r\n";
+Console.WriteLine(Cer3);
+
+Console.WriteLine("Languages");
+
+string Lan1 = "English";
+Console.WriteLine(Lan1);
+
+string Lan2 = "Spanish";
+Console.WriteLine(Lan2);
+
+string Lan3 = "Portuguese\r\n";
+Console.WriteLine(Lan3);
+
+Console.WriteLine("Experiences");
+
+string Exp1 = "Internship from Erasmus+ in Atriam Consulting SL(Malaga, Spain)(01.06.2022-01.09.2022)";
+Console.WriteLine(Exp1);
+
+string Exp11 = "-Taken photos, written essays and made videos for the adventures and the life in Malaga, Spain\r\n";
+Console.WriteLine(Exp11);
+
+string Exp2 = "Technical Support with Spanish in Sutherland Global Services Bulgaria(01.04.2023-01.02.2024)";
+Console.WriteLine(Exp2);
+
+string Exp22 = "-Technical support for customers in LATAM(America Latina) in Sutherland Bulgaria. Work with services of EERO(B2B project of Amazon) routers. Support customers with giving them topologies for the right connection of the routers with modems or not. \r\nConsulting them with the right information of the devices EERО.\r\n";
+Console.WriteLine(Exp22);
+
+string Exp3 = "Internship A1 Bulgaria (01.04.2024-01.02.2024)";
+Console.WriteLine(Exp3);
+
+string Exp33 = "-Work with Excel data of A1 Bulgaria. Studying presentations for devices related to DWDM and DWDM’s theory.\r\n";
+Console.WriteLine(Exp33);
+
+string Exp4 = "Junior Engineer in DWDM team for A1 Bulgaria(01.02.2025-11.11.2025)";
+Console.WriteLine(Exp4);
+
+string Exp44 = "-Work with DWDM data due to the inventory of A1 Bulgaria. How many pieces of technical devices are used and how many are out of service. Filling invoices from SAP into data folders of A1 Bulgaria. Filling lengths of the active fibers in Excel files of A1 Bulgaria. Making presentations for different DWDM devices of Huawei.\r\n";
+Console.WriteLine(Exp44);
