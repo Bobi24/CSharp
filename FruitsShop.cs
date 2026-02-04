0a3aef2241c968a5d15c3648f9229391f550e3e8
Borislav Motev <87946210+Bobi24@users.noreply.github.com>
2026-02-04 17:46:42 +0200

    Add files via upload

---
+++ FruitsShop.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double PriceKg = 1.4;
+
+Console.WriteLine("How much do you want to buy?");
+double Kg = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"The total sum is: {PriceKg*Kg}");
